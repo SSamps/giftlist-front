03b238b7d4a7292c242eff25b1dc09acb2052cc5
Simon <simonjsampson@gmail.com>
2021-07-19 15:27:53 +0100

    Added descriptions for new lists

---
+++ src/components/newList/content/listDescriptions/GiftListDescription.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GiftListDescription: React.FC = () => {
+    return (
+        <div className='newListDescription'>
+            Help them choose something you'd love without giving away any surprises
+        </div>
+    );
+};
+
+export default GiftListDescription;
--- src/components/newList/content/listDescriptions/ListDescriptionContainer.tsx
+++ src/components/newList/content/listDescriptions/ListDescriptionContainer.tsx
@@ -1,11 +1,30 @@
-import { TYPE_LIST_GROUP_ALL_TOP_LEVEL_VARIANTS } from '../../../../types/listVariants';
+import {
+    BASIC_LIST,
+    GIFT_GROUP,
+    GIFT_LIST,
+    TYPE_LIST_GROUP_ALL_TOP_LEVEL_VARIANTS,
+} from '../../../../types/listVariants';
+import BasicListDescription from './BasicListDescription';
+import GiftGroupDescription from './GiftGroupDescription';
+import GiftListDescription from './GiftListDescription';
 
 interface Props {
     controllerState: TYPE_LIST_GROUP_ALL_TOP_LEVEL_VARIANTS;
 }
 
+const renderDescription = (controllerState: TYPE_LIST_GROUP_ALL_TOP_LEVEL_VARIANTS) => {
+    switch (controllerState) {
+        case BASIC_LIST:
+            return <BasicListDescription></BasicListDescription>;
+        case GIFT_LIST:
+            return <GiftListDescription></GiftListDescription>;
+        case GIFT_GROUP:
+            return <GiftGroupDescription></GiftGroupDescription>;
+    }
+};
+
 const ListDescriptionContainer: React.FC<Props> = ({ controllerState }) => {
-    return <div className='newListDescriptionContainer'>{controllerState} Description</div>;
+    return <div className='newListDescriptionContainer'>{renderDescription(controllerState)}</div>;
 };
 
 export default ListDescriptionContainer;
