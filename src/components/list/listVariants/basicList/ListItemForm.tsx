825a3dc18f9ba0df94d58bd8c49ce8c4b9105f72
Simon <simonjsampson@gmail.com>
2021-07-31 20:41:41 +0100

    Refactored new list item form out to a separate component which can be reused in the modify item component.

---
+++ src/components/list/listVariants/basicList/ListItemForm.tsx
@@ -0,0 +1,163 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { newListItemActionCreator } from '../../../../redux/actions/listGroupActions';
+import Spinner from '../../../misc/spinner';
+
+interface Props {
+    submitFormData: (itemBody: string, itemLinks: string[]) => Promise<void>;
+    shouldCloseAfterSubmit: boolean;
+    submitButtonLabel: string;
+    setItemFormHidden: () => void;
+    providedInitialFormState?: any;
+    maxLinks: number;
+}
+
+interface InewItemState {
+    itemBody: string;
+    itemLinks: string[];
+    waiting: boolean;
+    error: string;
+}
+
+const ListItemForm: React.FC<Props> = ({
+    submitFormData,
+    shouldCloseAfterSubmit,
+    submitButtonLabel,
+    setItemFormHidden,
+    providedInitialFormState,
+    maxLinks,
+}) => {
+    let initialFormState;
+    if (providedInitialFormState) {
+        initialFormState = providedInitialFormState;
+    } else {
+        initialFormState = {
+            itemBody: '',
+            itemLinks: [''],
+            waiting: false,
+            error: '',
+        };
+    }
+
+    const [itemFormState, setItemFormState] = useState<InewItemState>(initialFormState);
+
+    const { itemBody, itemLinks, waiting } = itemFormState;
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setItemFormState({ ...itemFormState, [e.target.name]: e.target.value });
+    };
+
+    const onChangeLinks = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+        let userAddedChars = e.target.value.length > itemLinks[index].length;
+        let updatedItemLinks = Array.from(itemLinks);
+        updatedItemLinks[index] = e.target.value;
+
+        if (userAddedChars && index === itemLinks.length - 1 && itemLinks.length < maxLinks) {
+            updatedItemLinks.push('');
+        }
+
+        setItemFormState({ ...itemFormState, itemLinks: updatedItemLinks });
+    };
+
+    const addNewLinkField = () => {
+        let updatedItemLinks = Array.from(itemLinks);
+        updatedItemLinks.push('');
+        setItemFormState({ ...itemFormState, itemLinks: updatedItemLinks });
+    };
+
+    const removeLink = (index: number) => {
+        let updatedItemLinks = Array.from(itemLinks);
+        updatedItemLinks.splice(index, 1);
+        setItemFormState({ ...itemFormState, itemLinks: updatedItemLinks });
+    };
+
+    const submitForm = async () => {
+        let updatedItemLinks = Array.from(itemLinks);
+        for (let i = updatedItemLinks.length - 1; i >= 0; i--) {
+            if (updatedItemLinks[i].length === 0) {
+                updatedItemLinks.splice(i, 1);
+            }
+        }
+
+        setItemFormState({ ...itemFormState, itemLinks: updatedItemLinks, waiting: true, error: '' });
+        try {
+            await submitFormData(itemBody, updatedItemLinks);
+            if (shouldCloseAfterSubmit) {
+                setItemFormHidden();
+            } else {
+                setItemFormState({ itemBody: '', itemLinks: [''], waiting: false, error: '' });
+            }
+        } catch (err) {
+            setItemFormState({
+                ...itemFormState,
+                itemLinks: updatedItemLinks,
+                waiting: false,
+                error: err.response.status,
+            });
+        }
+    };
+
+    return (
+        <div className='basicListNewItemFormContainer'>
+            <form className='form'>
+                <div className='form-groupWithSideControls'>
+                    <label className='form-label'>Item</label>
+                    <div className='form-group-inputContainerWithSideControls'>
+                        <input type='text' name='itemBody' value={itemBody} onChange={onChange} required />
+                    </div>
+                </div>
+                <div className='form-groupWithSideControls'>
+                    <label className='form-label'>{'Link (optional)'}</label>
+                    <div className='form-group-inputContainerWithSideControls'>
+                        <input
+                            type='text'
+                            key='itemLinks0'
+                            name='itemLinks0'
+                            placeholder='none'
+                            value={itemLinks[0]}
+                            onChange={(e) => onChangeLinks(e, 0)}
+                        />
+                    </div>
+                    {itemLinks.map((_, index) => {
+                        if (index > 0) {
+                            return (
+                                <div className='form-group-inputContainerWithSideControls' key={'newItemLinks' + index}>
+                                    <input
+                                        type='text'
+                                        name={'newItemLinks' + index}
+                                        placeholder='none'
+                                        value={itemLinks[index]}
+                                        onChange={(e) => onChangeLinks(e, index)}
+                                    />
+                                    <i className='fas fa-minus btn-simple' onClick={() => removeLink(index)}></i>
+                                </div>
+                            );
+                        } else {
+                            return null;
+                        }
+                    })}
+                    {itemLinks.length < maxLinks && (
+                        <label
+                            className=' form-label form-label-offset btn-simple form-group-inputButtonBelow'
+                            onClick={addNewLinkField}
+                        >
+                            <i className='fas fa-plus btn-simple'></i> Additional Link
+                        </label>
+                    )}
+                </div>
+                <div className='form-controls'>
+                    <span className='btn btn-primary' onClick={submitForm}>
+                        {submitButtonLabel}
+                    </span>
+                    <span className='btn btn-primary' onClick={setItemFormHidden}>
+                        Cancel
+                    </span>
+                </div>
+            </form>
+
+            {waiting && <Spinner className='spinner-tiny'></Spinner>}
+        </div>
+    );
+};
+
+export default connect(null, { newListItemActionCreator })(ListItemForm);
--- src/components/list/listVariants/basicList/NewListItem.tsx
+++ src/components/list/listVariants/basicList/NewListItem.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { newListItemActionCreator, TnewListItemActionCreator } from '../../../../redux/actions/listGroupActions';
-import Spinner from '../../../misc/spinner';
+import ListItemForm from './ListItemForm';
 
 interface Props {
     itemType: 'listItem' | 'secretListItem';
@@ -9,84 +9,19 @@ interface Props {
     newListItemActionCreator: TnewListItemActionCreator;
 }
 
-interface InewItemState {
-    active: boolean;
-    newItemBody: string;
-    newItemLinks: string[];
-    waiting: boolean;
-    error: string;
-}
-
 const NewListItem: React.FC<Props> = ({ itemType, groupId, newListItemActionCreator }) => {
-    const initialNewItemState = {
-        active: false,
-        newItemBody: '',
-        newItemLinks: [''],
-        waiting: false,
-        error: '',
-    };
+    const [itemFormVisible, setitemFormVisible] = useState(false);
 
-    const [newItemState, setNewItemState] = useState<InewItemState>(initialNewItemState);
-
-    const { active, newItemBody, newItemLinks, waiting } = newItemState;
-    const maxLinks = 3;
-
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setNewItemState({ ...newItemState, [e.target.name]: e.target.value });
-    };
-
-    const onChangeLinks = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-        let userAddedChars = e.target.value.length > newItemLinks[index].length;
-        let updatedNewItemLinks = Array.from(newItemLinks);
-        updatedNewItemLinks[index] = e.target.value;
-
-        if (userAddedChars && index === newItemLinks.length - 1 && newItemLinks.length < maxLinks) {
-            updatedNewItemLinks.push('');
-        }
-
-        setNewItemState({ ...newItemState, newItemLinks: updatedNewItemLinks });
-    };
-
-    const submitForm = async () => {
-        let updatedNewItemLinks = Array.from(newItemLinks);
-        for (let i = updatedNewItemLinks.length - 1; i >= 0; i--) {
-            if (updatedNewItemLinks[i].length === 0) {
-                updatedNewItemLinks.splice(i, 1);
-            }
-        }
-
-        setNewItemState({ ...newItemState, newItemLinks: updatedNewItemLinks, waiting: true, error: '' });
-        try {
-            await newListItemActionCreator(newItemBody, updatedNewItemLinks, itemType, groupId);
-            setNewItemState({ active: true, newItemBody: '', newItemLinks: [], waiting: false, error: '' });
-        } catch (err) {
-            setNewItemState({
-                ...newItemState,
-                newItemLinks: updatedNewItemLinks,
-                waiting: false,
-                error: err.response.status,
-            });
-        }
+    const submitForm = async (itemBody: string, itemLinks: string[]) => {
+        await newListItemActionCreator(itemBody, itemLinks, itemType, groupId);
     };
 
     const showNewItemForm = () => {
-        setNewItemState({ ...newItemState, active: true });
-    };
-
-    const addNewLinkField = () => {
-        let updatedNewItemLinks = Array.from(newItemLinks);
-        updatedNewItemLinks.push('');
-        setNewItemState({ ...newItemState, newItemLinks: updatedNewItemLinks });
-    };
-
-    const removeLink = (index: number) => {
-        let updatedNewItemLinks = Array.from(newItemLinks);
-        updatedNewItemLinks.splice(index, 1);
-        setNewItemState({ ...newItemState, newItemLinks: updatedNewItemLinks });
+        setitemFormVisible(true);
     };
 
     const hideNewItemForm = () => {
-        setNewItemState(initialNewItemState);
+        setitemFormVisible(false);
     };
 
     const returnNewItemButton = () => {
@@ -96,74 +31,22 @@ const NewListItem: React.FC<Props> = ({ itemType, groupId, newListItemActionCrea
             </div>
         );
     };
-    const returnNewItemForm = () => {
-        return (
-            <div className='basicListNewItemFormContainer'>
-                <form className='form'>
-                    <div className='form-groupWithSideControls'>
-                        <label className='form-label'>Item</label>
-                        <div className='form-group-inputContainerWithSideControls'>
-                            <input type='text' name='newItemBody' value={newItemBody} onChange={onChange} required />
-                        </div>
-                    </div>
-                    <div className='form-groupWithSideControls'>
-                        <label className='form-label'>{'Link (optional)'}</label>
-                        <div className='form-group-inputContainerWithSideControls'>
-                            <input
-                                type='text'
-                                key='newItemLinks0'
-                                name='newItemLinks0'
-                                placeholder='none'
-                                value={newItemLinks[0]}
-                                onChange={(e) => onChangeLinks(e, 0)}
-                            />
-                        </div>
-                        {newItemLinks.map((_, index) => {
-                            if (index > 0) {
-                                return (
-                                    <div
-                                        className='form-group-inputContainerWithSideControls'
-                                        key={'newItemLinks' + index}
-                                    >
-                                        <input
-                                            type='text'
-                                            name={'newItemLinks' + index}
-                                            placeholder='none'
-                                            value={newItemLinks[index]}
-                                            onChange={(e) => onChangeLinks(e, index)}
-                                        />
-                                        <i className='fas fa-minus btn-simple' onClick={() => removeLink(index)}></i>
-                                    </div>
-                                );
-                            } else {
-                                return null;
-                            }
-                        })}
-                        {newItemLinks.length < maxLinks && (
-                            <label
-                                className=' form-label form-label-offset btn-simple form-group-inputButtonBelow'
-                                onClick={addNewLinkField}
-                            >
-                                <i className='fas fa-plus btn-simple'></i> Additional Link
-                            </label>
-                        )}
-                    </div>
-                    <div className='form-controls'>
-                        <span className='btn btn-primary' onClick={submitForm}>
-                            Add item
-                        </span>
-                        <span className='btn btn-primary' onClick={hideNewItemForm}>
-                            Cancel
-                        </span>
-                    </div>
-                </form>
 
-                {waiting && <Spinner className='spinner-tiny'></Spinner>}
-            </div>
-        );
-    };
-
-    return <Fragment>{!active ? returnNewItemButton() : returnNewItemForm()}</Fragment>;
+    return (
+        <Fragment>
+            {!itemFormVisible ? (
+                returnNewItemButton()
+            ) : (
+                <ListItemForm
+                    submitFormData={submitForm}
+                    shouldCloseAfterSubmit={false}
+                    submitButtonLabel='Add item'
+                    setItemFormHidden={hideNewItemForm}
+                    maxLinks={3}
+                ></ListItemForm>
+            )}
+        </Fragment>
+    );
 };
 
 export default connect(null, { newListItemActionCreator })(NewListItem);
