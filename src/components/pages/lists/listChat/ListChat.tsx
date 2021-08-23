5d573e3326504f15f6058655c19dfae6224bef45
Simon <simonjsampson@gmail.com>
2021-08-23 15:06:56 +0100

    Fixed bug in ListChat where duplicate event listenders were being created.

--- src/components/pages/lists/listChat/ListChat.tsx
+++ src/components/pages/lists/listChat/ListChat.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import io, { Socket } from 'socket.io-client';
 import { DefaultEventsMap } from 'socket.io-client/build/typed-events';
@@ -17,6 +17,8 @@ interface Props {
 const GiftListChat: React.FC<Props> = ({ ownerName, token, currentList }) => {
     const [socket, setSocket] = useState<undefined | Socket<DefaultEventsMap, DefaultEventsMap>>(undefined);
     const [messages, setMessages] = useState<TmessageAny[]>([]);
+    const latestMessageRef = useRef<HTMLDivElement>(null);
+    const chatContainerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         const socket = io(process.env.REACT_APP_BACKEND_BASE_URL || 'http://localhost:5000', {
@@ -31,31 +33,30 @@ const GiftListChat: React.FC<Props> = ({ ownerName, token, currentList }) => {
         socket.emit('giftListChat:joinRoom');
 
         socket.on('giftListChat:joinRoom-success', (currentMessages) => {
+            console.log('setting messages on joining the room');
             setMessages([...currentMessages]);
+        });
+
+        socket.on('giftListChat:postMessage-success', (newMessage) => {
+            console.log('setting messages ofter posting');
+            setMessages((messages) => [...messages, newMessage]);
         });
 
         setSocket(socket);
     }, []);
 
-    if (socket) {
-        socket.on('giftListChat:postMessage-success', (newMessage) => {
-            setMessages([...messages, newMessage]);
-        });
-    }
-
-    const renderChatVisibilityMessage = () => {
-        return (
-            <div className='systemMessage'>
-                <i className='fas fa-eye-slash danger'></i>{' '}
-                <span>
-                    {ownerName} <strong>can't</strong> see your chat
-                </span>
-            </div>
-        );
-    };
+    useEffect(() => {
+        scrollToLatestMessage();
+    }, [messages]);
 
     const submitForm = (body: string) => {
         socket && socket.emit('giftListChat:postMessage', body);
+    };
+
+    const scrollToLatestMessage = () => {
+        if (chatContainerRef.current && latestMessageRef.current) {
+            chatContainerRef.current.scroll(0, latestMessageRef.current.offsetTop);
+        }
     };
 
     const renderMessages = () => {
@@ -64,14 +65,23 @@ const GiftListChat: React.FC<Props> = ({ ownerName, token, currentList }) => {
                 {messages.map((message) => {
                     return <ListChatMessage key={message._id} message={message}></ListChatMessage>;
                 })}
+                <div ref={latestMessageRef}></div>
             </Fragment>
         );
     };
+
     return (
         <div className='listSectionContainer listChatContainerBorder'>
             <div className='listSectionContentContainer'>
-                {renderChatVisibilityMessage()}
-                <div className='listChat'>{renderMessages()}</div>
+                <div className='systemMessage'>
+                    <i className='fas fa-eye-slash danger'></i>
+                    <span>
+                        {ownerName} <strong>can't</strong> see your chat
+                    </span>
+                </div>
+                <div ref={chatContainerRef} className='listChat'>
+                    {renderMessages()}
+                </div>
                 <ListChatForm submitForm={submitForm}></ListChatForm>
             </div>
         </div>
